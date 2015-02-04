cacfb0437b8f3be70d217ab26a9ddde25296b867
sampsep <saminsepasi@gmail.com>
2015-02-03 19:27:21 -0800

    resolve conflict

--- client/scoreApp.js
+++ client/scoreApp.js
@@ -1,5 +1,7 @@
-
-scoreApp = angular.module('scoreApp')
-  .controller('scoreCtrl', function($rootscope, $scope) {
+// 'use strict';
+var scoreApp = angular.module('scoreApp', [])
+  .controller('scoreCtrl', function($scope) {
   $scope.score = 0;
+  $scope.addScore = function() {};
+  $scope.subtractScore = function() {};
 });
