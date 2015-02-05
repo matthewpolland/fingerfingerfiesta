c29c5d52af87002e474406b23032ecaea2d31288
sampsep <saminsepasi@gmail.com>
2015-02-05 15:02:08 -0800

    (feat) score check

---
+++ client/scoreApp.js
@@ -0,0 +1,5 @@
+scoreApp = angular.module('scoreApp', [])
+  .controller('scoreCtrl', function($scope) {
+    $scope.score = totalScore;
+    $scope.minScore = minScore;
+  })
--- client/scoreMethods.js
+++ client/scoreMethods.js
@@ -1,17 +1,21 @@
 var totalScore = 0;
+var minScore = 20;
 var scoreMethods = {
     // score: 0,
     addScore: function() {
       console.log('this is score', totalScore)
       totalScore += 5;
-      $('h1').text("Score: " + ""+totalScore+"");
+       $('h1').text("Score: " + ""+totalScore+""+"/"+""+minScore+"");
+      if (checkScore === true) {
+        alert('you passed!');
+      }
     },
     subtractScore: function() {
-      $('h1').text("Score: " + ""+totalScore+"");
+      $('h1').text("Score: " + ""+totalScore+""+"/"+""+minScore+"");
       totalScore -= 5;
     },
-    getScore: function() {
-      return totalScore;
+    checkScore: function() {
+      return totalScore >= minScore;
     }
 }
 
