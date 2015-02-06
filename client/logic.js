dd8f176249cf26584f7717275451957cfb5735a7
sampsep <saminsepasi@gmail.com>
2015-02-05 19:11:37 -0800

    (feat) level incrementing

--- client/logic.js
+++ client/logic.js
@@ -1,33 +1,9 @@
-
-var Xcheckline = function(touchesStoreX, touchesStoreY, ctx){
-  var beginX = touchesStoreX[0];
-  var beginY = touchesStoreY[0];
-  var endX = touchesStoreX[touchesStoreX.length-1];
-  var endY = touchesStoreY[touchesStoreX.length-1];
-  var slope = -(endX-beginX)/(endY-beginY);
-  console.log('SLOPE', slope);
-
-  var maxHeight = 900;
-  var minHeight = 700;
-  var mustBeLeftOf = 375;
-  var mustBeRightOf = 625;
-  if(beginX<=mustBeLeftOf && endX>=mustBeRightOf){
-    for(var i = 0; i < touchesStoreY.length; i++){
-      // console.log(i, touchesStoreY[i])
-      if(touchesStoreY[i] < maxHeight && touchesStoreY[i] > minHeight){
-        // console.log("YAY!!!");
-        // scoreMethods.addScore();
-        //below clears the line 
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-        //check if line hits square HERE
-        //ctx.clearRect(square.x,square.y,square.w,square.h);
-      }
-    }
-  }
-}
-
 var timeUp = false;
 var level = 0;
+var levels = [
+{minScore: 20, color: purple},
+{minscore: 40, color: green}
+]
 
 var timer = function () {
   timeUp = false;
@@ -59,7 +35,9 @@ var endLevel = function (didYouWin) {
 };
 
 
-var initLevel = function (level) {
-  timer();
+var initLevel = function () {
+  // timer();
+  // console.log('this is color', levels[lvl][color])
+  // createRect(ctx, levels[lvl][color]);
   //run the box spawner
 };
