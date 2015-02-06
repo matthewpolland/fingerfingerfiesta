b2da7fa48a90ed56261050d7ef40856a6fa615f5
Jake Obron <jbobron@gmail.com>
2015-02-06 11:39:01 -0800

    (feat) add timer in logic and html

--- client/logic.js
+++ client/logic.js
@@ -4,33 +4,33 @@ var levels = [
 {minScore: 20, color: "purple", timer: 5000},
 {minscore: 40, color: "green", timer: 3000}
 ]
+var time = 30;
+var didYouWin = true;
+
+var getTime = function(){
+  $('h2').text("Time: " + time);
+  time--;
+  if(time === 0){
+    didYouWin = false;
+    endLevel(didYouWin);
+    //stop timer
+  }
+}
+
 
 var timer = function () {
-  timeUp = false;
-  var now = Date.now();
-  var end = now + 30000;
-  var didYouWin = true;
-  while(now < end){
-    now = Date.now();
-  }
-  //if timer reaches zero
-  didYouWin = false;
-  timeUp = true; 
-  alert('Time Up!');
-  endLevel(didYouWin); //pass it loss condition
+  setInterval(getTime, 1000);
 };
 
 var endLevel = function (didYouWin) {
   //render something depending on win state
   //clear screen
   if(didYouWin){
-    alert('You Win! Get ready for the next level!');
-    var nextLevel = level + 1;
-    console.log(nextLevel);
-    console.log(levels[nextLevel]);
-    startup(levels[nextLevel]);
+    // alert('You Win! Get ready for the next level!');
+    var nextLevel = level++;
+    //startup();;
   }else{
-    alert('You Lose! Back to Level 1');
+    // alert('You Lose! Back to Level 1');
     level = 0;
     //startup();
   }
@@ -38,7 +38,13 @@ var endLevel = function (didYouWin) {
 
 
 var initLevel = function (ctx, level) {
-  //timer();
+  timer();
   var finalCountDown = addRect(ctx, level.color, level.timer);
   setInterval(finalCountDown,33);
 };
+
+
+
+
+
+
